2edf610878f879bf6e628182a8fdb9766191d788
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-08-22 13:54:14 +0100

    Corrected app.js routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
+import Home from './containers/home'
+import About from './containers/about'
+import Contact from './containers/contact'
+import Portfolio from './containers/portfolio'
+import Resume from './containers/resume'
+import Skills from './containers/skills'
+
 
 function App() {
   return (
