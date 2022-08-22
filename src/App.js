1fcc4198e73ae85ee6af04959e52035871c3f361
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-08-22 13:43:40 +0100

    Adding basic routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      {/* particles js */}
+
+      {/* navbar component */}
+
+      {/* main page content */}
+      <Routes>
+        {/* create all the routes here */}
+
+        
+      </Routes>
       
     </div>
   );
