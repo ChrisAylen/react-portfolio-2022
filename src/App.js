ceecdbf523f8e31719c020a1ec28ce601b36e7a8
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-08-23 12:47:23 +0100

    Added particlles on home page, with 2 congfigurations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './containers/home'
 import About from './containers/about'
 import Contact from './containers/contact'
@@ -8,11 +8,24 @@ import Resume from './containers/resume'
 import Skills from './containers/skills'
 import Navbar from './components/navBar'
 
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import particlesConfig from './helpers/particlesConfig';
+
+
 
 function App() {
+  const particlesInit=async (main)=>{
+    await loadFull(main);
+  }
+  const location = useLocation();
+  const renderOnlyOnHomePage = location.pathname === '/';
+
   return (
     <div className="App">
       {/* particles js */}
+      {renderOnlyOnHomePage && <Particles className="particles" params={particlesConfig} init={particlesInit} />}
+
 
       {/* navbar component */}
       <Navbar/>
