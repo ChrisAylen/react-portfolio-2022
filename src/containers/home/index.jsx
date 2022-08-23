b1617a1e5e34e6aa2a0888d57186a8bd758bea62
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-08-23 13:59:51 +0100

    Added buttons and social links (not completed)

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import './styles.scss';
+import "./styles.scss";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -7,8 +8,19 @@ const Home = () => {
       <div className="home__text-wrapper">
         <h1>
           Hello, I'm Chris
-          <br />full stack extremo.
+          <br />
+          full stack extremo.
         </h1>
+      </div>
+      <div className="contact-me">
+        <div className="contact-me__buttons-wrapper">
+          <button>Hire me</button>
+          <a>Download CV</a>
+        </div>
+        <div className="contact-me__socials-wrapper">
+            <FaLinkedin size={32} />
+            <FaGithub size={32} />
+        </div>
       </div>
     </section>
   );
