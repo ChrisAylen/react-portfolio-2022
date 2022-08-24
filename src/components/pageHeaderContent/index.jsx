a09187a22236b7885daefe78529cf1512e0272c0
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-08-24 10:37:10 +0100

    started page header component

---
+++ src/components/pageHeaderContent/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles.scss';
+
+const PageHeaderContent = (props) => {
+    const { headerText, icon } = props;
+
+    return(
+        <div className="wrapper">
+            <h2></h2>
+
+        </div>
+    )
+}
+
+export default PageHeaderContent;
