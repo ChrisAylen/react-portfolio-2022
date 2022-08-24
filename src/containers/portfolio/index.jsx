b5908ac77a083524028d55363fab67dd8f8c2387
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-08-24 20:45:35 +0100

    Adding portfolio code - image gallery

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,11 +1,48 @@
 import React from "react";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { AiFillProject } from "react-icons/ai";
+import './styles.scss'
+
+import { filterOptions, portfolioData } from "./utils";
 
 const Portfolio = () => {
-    return (
-        <div>
-            Portfolio
+  return (
+    <section className="portfolio" id="portfilio">
+      <PageHeaderContent
+        headerText="Portfolio of Work"
+        icon={<AiFillProject size={40} />}
+      />
+
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+            {
+                filterOptions.map((option)=>
+                <li key={option.id}>
+                        {option.label}
+                </li>
+                )
+            }
+
+
+        </ul>
+        <div className="portfolio__content__cards">
+            {
+                portfolioData.map((item)=>(
+                    <div key={item.sectionId} className="portfolio__content__cards__item">
+                        <div className="portfolio__content__cards__item__image-wrapper">
+                            <a>
+                                <img src={item.image} alt="project image"/>
+                            </a>
+                        </div>
+
+                    </div>
+
+                ))
+            }
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
---
+++ src/containers/portfolio/utils.js
@@ -0,0 +1,74 @@
+import Image1 from "../../images/image1.PNG";
+import Image2 from "../../images/image2.jpg";
+import Image3 from "../../images/image3.png";
+import Image4 from "../../images/image4.png";
+import Image5 from "../../images/image5.png";
+import Image6 from "../../images/image6.png";
+import Image7 from "../../images/image7.PNG";
+
+export const portfolioData = [
+    {
+        sectionId: '2',
+        projecName: 'Project Bank Heist',
+        projectLink: 'https://github.com/ChrisAylen/project-bank-heist',
+        image: Image1
+
+    },
+    {
+        sectionId: '2',
+        projecName: 'Conan Launcher',
+        projectLink: 'https://www.google.com',
+        image: Image2
+
+    },
+    {
+        sectionId: '2',
+        projecName: 'Daily Planner',
+        projectLink: 'https://www.google.com',
+        image: Image3
+
+    },
+    {
+        sectionId: '2',
+        projecName: 'Movie Translator',
+        projectLink: 'https://www.google.com',
+        image: Image4
+
+    },
+    {
+        sectionId: '2',
+        projecName: 'Note Taker',
+        projectLink: 'https://www.google.com',
+        image: Image5
+
+    },
+    {
+        sectionId: '3',
+        projecName: 'Password Generator',
+        projectLink: 'https://www.google.com',
+        image: Image6
+
+    },    {
+        sectionId: '3',
+        projecName: 'Weather Dashboard',    
+        projectLink: 'https://www.google.com',
+        image: Image7
+
+    },
+]
+
+export const filterOptions = [
+    {
+        label: 'All',
+        id: 1
+    },
+    {
+        label: 'Development',
+        id: 2
+    },
+    {
+        label: 'Projects',
+        id: 3
+    },
+
+];
