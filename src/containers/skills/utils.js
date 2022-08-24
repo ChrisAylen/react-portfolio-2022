e1175705c34cdfa604e0fa07827792d71651d7da
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-08-24 15:34:03 +0100

    Added the skills content and features

---
+++ src/containers/skills/utils.js
@@ -0,0 +1,58 @@
+export const skillsData = [
+    {
+        skillType: "Front-End",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "80"
+            },
+            {
+                skillName: "CSS",
+                percentage: "40"
+
+            },
+            {
+                skillName: "JavaScript",
+                percentage: "90"
+            }
+        ]
+    },
+    {
+        skillType: "Back-End",
+        data: [
+            {
+                skillName: "C#",
+                percentage: "80"
+            },
+            {
+                skillName: "Node.js",
+                percentage: "90"
+
+            },
+            {
+                skillName: "Vb.net",
+                percentage: "70"
+            },
+
+        ]
+    },
+    {
+        skillType: "Databases",
+        data: [
+            {
+                skillName: "SQL Server",
+                percentage: "80"
+            },
+            {
+                skillName: "MongoDB",
+                percentage: "90"
+
+            },
+            {
+                skillName: "MYSQL",
+                percentage: "80"
+            },
+
+        ]
+    }
+];
